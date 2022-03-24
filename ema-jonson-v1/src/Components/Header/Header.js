8669c5775597a96ca90b08dd4853cd8336940dda
anamerasul <anamerasul@gmail.com>
2022-03-24 18:45:55 +0600

    Header Component, logo position, set nav menu

--- ema-jonson-v1/src/Components/Header/Header.js
+++ ema-jonson-v1/src/Components/Header/Header.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './Header.css'
+import logo from './../../images/Logo.svg'
 
 const Header = () => {
     return (
         <div>
-            <h1>This is header</h1>
+            <nav className="header">
+                <img src={logo} alt="" />
+
+                <div>
+                    <a href="/shop">Shop</a>
+                    <a href="/order">Order</a>
+                    <a href="/inventory">Inventory</a>
+                    <a href="/about">About</a>
+                </div>
+            </nav>
         </div>
     );
 };
