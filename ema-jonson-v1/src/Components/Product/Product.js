a227f3ab1d9f983a856050d168e6a40d47b35cf8
anamerasul <anamerasul@gmail.com>
2022-03-24 19:53:54 +0600

    Style Product Cart, style debug and button position

--- ema-jonson-v1/src/Components/Product/Product.js
+++ ema-jonson-v1/src/Components/Product/Product.js
@@ -8,8 +8,16 @@ const Product = (props) => {
     return (
         <div className='product'>
             <img src={img} alt='Product'></img>
+            <div className="product-info">
             <p className='product-name'>{name}</p>
-            
+            <p><small>Price:${price}</small></p>
+            <p><small>Seller:{seller}</small></p>
+            <p><small>Ratings:{ratings}</small></p>
+
+            </div>
+            <button className='btn-cart'>
+                <p>Add to cart</p>
+            </button>
         </div>
     );
 };
